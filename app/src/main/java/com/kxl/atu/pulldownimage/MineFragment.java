3d5806395d634b1f632f069f229e5c4448fff89a
www_zzti@163.com <5877076hn>
2016-10-11 16:48:55 +0800

    Initial commit

---
+++ app/src/main/java/com/kxl/atu/pulldownimage/MineFragment.java
@@ -0,0 +1,170 @@
+package com.kxl.atu.pulldownimage;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.kxl.atu.pulldownimage.zoom.PullToZoomScrollViewEx;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MineFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MineFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MineFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+    private PullToZoomScrollViewEx scrollView;
+
+    public MineFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MineFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MineFragment newInstance(String param1, String param2) {
+        MineFragment fragment = new MineFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_mine, container, false);
+        scrollView = (PullToZoomScrollViewEx) view.findViewById(R.id.scroll_view);
+        loadViewForCode();
+        scrollView.setParallax(false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+    private void loadViewForCode() {
+        scrollView = (PullToZoomScrollViewEx) view.findViewById(R.id.scroll_view);
+        View headView = LayoutInflater.from(getContext()).inflate(R.layout.profile_head_view, null, false);
+        View zoomView = LayoutInflater.from(getContext()).inflate(R.layout.profile_zoom_view, null, false);
+        View contentView = LayoutInflater.from(getContext()).inflate(R.layout.profile_content_view, null, false);
+        scrollView.setHeaderView(headView);
+        scrollView.setZoomView(zoomView);
+        scrollView.setScrollContentView(contentView);
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.scroll_view, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            return true;
+        }
+//        else if (id == R.id.action_settings) {
+//            loadViewForCode();
+//            return true;
+//        }
+        else if (id == R.id.action_normal) {
+            scrollView.setParallax(false);
+            return true;
+        } else if (id == R.id.action_parallax) {
+            scrollView.setParallax(true);
+            return true;
+        } else if (id == R.id.action_show_head) {
+//            scrollView.showHeaderView();
+            scrollView.setHideHeader(false);
+            return true;
+        } else if (id == R.id.action_hide_head) {
+//            scrollView.hideHeaderVie˛w();
+            scrollView.setHideHeader(true);
+            return true;
+        } else if (id == R.id.action_disable_zoom) {
+//            scrollView.setEnableZoom(false);
+            scrollView.setZoomEnabled(false);
+            return true;
+        } else if (id == R.id.action_enable_zoom) {
+//            scrollView.setEnableZoom(true);
+            scrollView.setZoomEnabled(true);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
